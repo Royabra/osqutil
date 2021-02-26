70bf3b7a3f8e5ad3959df54bb264c8382e2d265d
Royabra <78696440+Royabra@users.noreply.github.com>
2021-02-26 11:51:47 +0200

    Update file.go

--- file.go
+++ file.go
@@ -7,30 +7,28 @@ import (
 
 // Outputs a basic file query
 func BasicFileQuery(szFilepath string, bCalcHash bool) string {
-	bIsFile := strings.ContainsAny(szFilepath, ".")
+	var szOperator string
+	if strings.ContainsAny(szFilepath, ".") {
+		szOperator = "="
+	} else {
+		szFilepath = strings.ReplaceAll(szFilepath, "*", "%")
+		szOperator = " LIKE "
+	}
 	szBasicFileQuery := "SELECT "
 
 	if !CheckPath(szFilepath) {
 		fmt.Println("WARNING! this path wasn't found on your machine, are you sure it's spelled correctly? \n")
 	}
 
-	if bIsFile && bCalcHash {
+	if bCalcHash {
 		szBasicFileQuery = szBasicFileQuery + "datetime(f.mtime, 'unixepoch', 'localtime') AS last_edited" +
 			", datetime(f.atime, 'unixepoch', 'localtime') AS last_accessed," +
 			"datetime(f.btime, 'unixepoch', 'localtime') AS creation_time, h.sha1," +
-			"f.path FROM file f JOIN hash h ON f.path=h.path WHERE path='" + szFilepath + "';"
-	} else if bIsFile {
-		szBasicFileQuery = szBasicFileQuery + "datetime(mtime, 'unixepoch', 'localtime') AS last_edited, " +
-			"datetime(atime, 'unixepoch', 'localtime') AS last_accessed, datetime(btime, 'unixepoch', 'localtime') " +
-			"AS creation_time, path FROM file WHERE path='" + szFilepath + "';"
-	} else if bCalcHash {
-		szBasicFileQuery = szBasicFileQuery + "datetime(f.mtime, 'unixepoch', 'localtime') AS last_edited, " +
-			"datetime(f.atime, 'unixepoch', 'localtime') AS last_accessed, datetime(f.btime, 'unixepoch', 'localtime')" +
-			" AS creation_time, h.sha1, f.path FROM file f JOIN hash h ON f.path=h.path WHERE path LIKE '" + strings.ReplaceAll(szFilepath, "*", "%") + "%';"
+			"f.path FROM file f JOIN hash h ON f.path=h.path WHERE path" + szOperator + "'" + szFilepath + "';"
 	} else {
 		szBasicFileQuery = szBasicFileQuery + "datetime(mtime, 'unixepoch', 'localtime') AS last_edited, " +
 			"datetime(atime, 'unixepoch', 'localtime') AS last_accessed, datetime(btime, 'unixepoch', 'localtime') " +
-			"AS creation_time, path FROM file WHERE path LIKE '" + strings.ReplaceAll(szFilepath, "*", "%") + "%';"
+			"AS creation_time, path FROM file WHERE path" + szOperator + "'" + strings.ReplaceAll(szFilepath, "*", "%") + "';"
 	}
 
 	return szBasicFileQuery
